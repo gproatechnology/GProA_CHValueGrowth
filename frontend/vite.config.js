59330af4c564551ca20e7ebf4f496daaa77a2ecd
Abdel <trejulu66@gmail.com>
2026-04-10 21:49:24 -0600

    fix: Set base to /app/ for production path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    base: '/',
+    base: '/app/',
     build: {
         outDir: '../static',
         emptyOutDir: true,
