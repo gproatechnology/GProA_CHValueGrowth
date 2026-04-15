6404d93b24ced53a4e4ead880fe1a08dd00d7bee
Tu Nombre <tu.email@ejemplo.com>
2026-04-14 18:48:27 -0600

    Fix: use absolute paths in vite build for production

--- static/assets/panel-left-open-6MFBJVuN.js
+++ static/assets/panel-left-open-6MFBJVuN.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CFfwiP8j.js";/**
+import{c as e}from"./index-CRL25sX9.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- static/assets/star-C2cfMXdq.js
+++ static/assets/star-C2cfMXdq.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CFfwiP8j.js";/**
+import{c as a}from"./index-CRL25sX9.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
